eeba647753b16c0f186beea465c42789cb511ead
NaumanMunir9 <naumanmunir9@gmail.com>
2021-11-04 19:14:58 +0500

    graphql query

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,28 @@
 // libraries
 import React from "react";
+import { API } from "aws-amplify";
 
 export default function Home() {
+  const getTodosQuery = `{
+    query GetTodos {
+      getTodos {
+        id
+        title
+        done
+      }
+    }
+  }`;
+
+  const fetchTodos = async () => {
+    try {
+      const data = await API.graphql({
+        query: getTodosQuery,
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  };
+
   return <div>Hello world!</div>;
 }
